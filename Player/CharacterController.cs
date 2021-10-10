93bac321147a4236512b3c398bbd1275b65fe9c9
GorIspolkom <eremin.artem.andreevich@gmail.com>
2021-10-10 20:53:55 +0300

    Player animation

--- Player/CharacterController.cs
+++ Player/CharacterController.cs
@@ -10,17 +10,17 @@ namespace Assets.Scripts.Players
 {
     public class CharacterController : BaseCharacterController
     {
-        //private static BaseCharacterController _instance;
-        //public static BaseCharacterController Instance => _instance;
         private static CharacterController _instance;
         public static CharacterController Instance => _instance;
         private Vector3 _direction;
+        private Animator _anim;
 
         public override void Awake()
         {
             base.Awake();
             _instance = this;
             _direction = Vector3.forward;
+            _anim = GetComponent<Animator>();
         }
         public override void Update()
         {
@@ -32,10 +32,17 @@ namespace Assets.Scripts.Players
         protected override void HandleInput()
         {
             if (Input.GetKey(KeyCode.G))
+            {
                 moveDirection = Vector3.zero;
+                _anim.SetBool("Run", false);
+            }
             else
+            {
                 moveDirection = _direction;
+                _anim.SetBool("Run", true);
+            }
             jump = Input.GetButton("Jump");
+            Debug.Log("Movement");
         }
 
         public void ChangeDirection() =>
